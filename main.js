b12ed7ff0359c2136df9977f5dca918ace10fcb4
unknown <qehreman.aliyeff@gmail.com>
2024-02-13 04:23:31 +0400

    son

--- main.js
+++ main.js
@@ -96,13 +96,52 @@ const suallar = [
     }
 ]
 
-let index = 0;
+
+function changeProgressBar() {
+    const percentage = 100 * (index / suallar.length)
+    document.querySelector('.prog-bar').style.width = percentage + '%'
+}
+
+let index = 0
+let correctAnswer = 0
 function showQuestion() {
-    const sual = suallar[index];
-    document.querySelector("#suallar").innerHTML=sual.sual;
-    document.querySelector("#first-answer").innerHTML=cavablar
+    const sual = suallar[index]
+    document.querySelector("#suallar").innerHTML = sual.sual
+    for (let i = 0; i < sual.cavablar.length; i++) {
+        document.querySelector('.variant' + (i + 1)).style.backgroundColor = '#343a40'
+        const btn = document.querySelector('.answer' + (i + 1))
+        btn.innerHTML = sual.cavablar[i].cavab
+        btn.onclick = function () {
+            if (sual.cavablar[i].dogrudurmu) {
+                document.querySelector('.variant' + (i + 1)).style.backgroundColor = 'green'
+                correctAnswer++
+                console.log(correctAnswer)
+            } else {
+                document.querySelector('.variant' + (i + 1)).style.backgroundColor = 'red'
+            }
+            setTimeout(next, 1000)
+            setTimeout(changeProgressBar, 1000)
+        }
+    }
+
 }
 
-function next () {
-
+function next() {
+    index++
+    const number = document.querySelector('#suall')
+    number.innerHTML = `Question ${index + 1}`
+    if (index > suallar.length - 1) {
+        number.innerHTML = `Question 1`
+        index = 0
+        document.querySelector('.variant1').style.backgroundColor = '#343a40'
+        document.querySelector('.variant2').style.backgroundColor = '#343a40'
+        document.querySelector('.variant3').style.backgroundColor = '#343a40'
+        document.querySelector('#first-page').style.display = 'block'
+        document.querySelector('#second-page').style.display = 'none'
+        const result = document.querySelector('#p1')
+        result.innerHTML = `Total points : ${(correctAnswer * 100) / suallar.length}%`
+    } else {
+        showQuestion()
+    }
 }
+showQuestion()
